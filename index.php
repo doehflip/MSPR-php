da17dfa282e734510862ffef2701714ed767134e
doehflip <jean.bouloc@ecoles-wis.net>
2021-11-22 12:17:41 +0100

    22/11 fin de séance

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include("./header-footer/header.php");
 ?>
 <div class="container-main">
-<h1 class="test">Kayak ou paddle, louer nos équipements pour la journée</h1>    
-<img class="image-fond" src="./images/standup-paddle.jpeg" alt="">
-
+    <h1 class="test">Kayak ou paddle, louer nos équipements pour la journée</h1>
+    <button class="cta-hp"><a href="./demande-reserv.php">Réservez maintenant!</a></button>    
+    <img class="image-fond" src="./images/standup-paddle.jpeg" alt="">
 </div>
 
 
